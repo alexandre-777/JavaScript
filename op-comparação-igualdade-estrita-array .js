9a78b5f712815e44d150745103f7cc7dba0db282
alexandre-777 <santanaalexandre1955@gmail.com>
2026-09-15 16:27:42 -0300

    add operador de comparação de igualdade simple array em JavaScript

---
+++ op-comparação-igualdade-estrita-array .js
@@ -0,0 +1,9 @@
+//listaA é um array com 5 elementos
+let listaA = [10, 20, 30 , 40, 50];
+
+//listaB é outro array com os mesmo 5 elementos que a lista!
+let listaB = [10,20,30,40,50];
+
+let listaC = listaA
+
+console.log(listaA === listaC); 
