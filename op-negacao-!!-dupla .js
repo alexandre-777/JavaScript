4beb7d8e43f4b54cff55272514a993e6553b4462
alexandre-777 <santanaalexandre1955@gmail.com>
2026-09-15 17:06:00 -0300

    add operador de disjunção !! simples em JavaScript

---
+++ op-negacao-!!-dupla .js
@@ -0,0 +1,11 @@
+//valores falsy
+console.log(!!0);  //false
+console.log(!!"");  //false
+console.log(!!null);  //false
+
+//valores truthy
+console.log(!!1)  //true
+console.log(!!"texto")//true     
+console.log(!![]) //array vazios são objetos, logo são truthy)
+console.log(!!{})//true (objeto vazios são true
+
