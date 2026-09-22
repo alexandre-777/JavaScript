01de267a3468bfeb96ed73a5b66bf60cf4877357
alexandre-777 <santanaalexandre1955@gmail.com>
2026-09-22 15:18:30 -0300

    add if em JavaScript

---
+++ if .js
@@ -0,0 +1,7 @@
+let hora = 15;
+
+if(hora < 12) {
+    console.log("Bom Dia magnata!");
+}
+//O restante do código continua indenpendente se a condição for verdadeira ou não
+console.log("Dinheiro não traz felicidades. Me dê o seu e seja feliz!😁")
